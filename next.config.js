c224cfec4eb6a537991556b2f3f06da7a902de7f
KairaT2007 <kairat.keihatsu@gmail.com>
2023-08-31 16:28:23 +0600

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["misc.scdn.co", "i.scdn.co", "geo-media.beatsource.com", "i1.sndcdn.com", "media.pitchfork.com", "dlmsvnqhygdaexejgmvc.supabase.co"]
+    domains: ["dlmsvnqhygdaexejgmvc.supabase.co"]
   }
 }
 
